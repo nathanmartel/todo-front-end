e58e908ff20d5c49ea1c436d68d08ecec5ae230e
Nathan Martel <nathanmartel@gmail.com>
2020-02-26 18:24:25 -0800

    login page fundamentals working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import Home from './Home.js';
+import Login from './Login.js';
 import TodoEdit from './TodoEdit.js';
 import './App.css';
 
@@ -8,8 +9,9 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/:name?" component={Home} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/edit/:id" component={TodoEdit} />
+        <Route exact path="/:name?" component={Home} />
       </Switch>
     </Router>
   );
---
+++ src/Login.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import request from 'superagent';
+
+export default class Login extends Component {
+    
+    state = {
+        emailSignUp: '',
+        passwordSignUp: '',
+        display_nameSignUp: '',
+        emailLogin: '',
+        passwordLogin: '',
+    }
+
+    handleSignUp = async () => {
+        const URL=`${process.env.REACT_APP_DB_URL}/api/auth/signup`;
+        const newUser = {
+            email: this.state.emailSignUp,
+            password: this.state.passwordSignUp,
+            display_name: this.state.display_nameSignUp,
+        }
+        const result = await request.post(URL, newUser);
+        console.log(result);
+    }
+    
+    handleLogin = async () => {
+        const URL=`${process.env.REACT_APP_DB_URL}/api/auth/signin`;
+        const newUser = {
+            email: this.state.emailLogin,
+            password: this.state.passwordLogin,
+        }
+        const result = await request.post(URL, newUser);
+        console.log(result);
+    }
+    
+    render() {
+        return (
+            <div>
+                <div className='signup-container'>
+                    <input onChange={(e) => this.setState({ emailSignUp: e.target.value })} value={this.state.emailSignUp} />
+                    <input onChange={(e) => this.setState({ display_nameSignUp: e.target.value })} value={this.state.display_nameSignUp} />
+                    <input onChange={(e) => this.setState({ passwordSignUp: e.target.value })} value={this.state.passwordSignUp} />
+                    <button onClick={this.handleSignUp}>Sign Up</button>
+                </div>
+                <div className='signin-container'>
+                    <input onChange={(e) => this.setState({ emailLogin: e.target.value })} value={this.state.emailLogin} />
+                    <input onChange={(e) => this.setState({ passwordLogin: e.target.value })} value={this.state.passwordLogin} />
+                    <button onClick={this.handleLogin}>Login</button>
+                </div>
+            </div>
+        )
+    }
+}
